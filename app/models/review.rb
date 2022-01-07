a16d9e723976027af1d0542443b82ba92357eec9
Matt Casperson <mc.casperson@gmail.com>
2022-01-07 10:46:12 -0800

    add validations for products and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates_length_of :content_body, in: 50..250
+  validates :rating, presence: true
+  validates_numericality_of :rating, greater_than: 0, less_than: 6
 end
