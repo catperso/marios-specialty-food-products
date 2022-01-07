adb042f279687ca88c98efd1caa3b9d0dfecb776
Matt Casperson <mc.casperson@gmail.com>
2022-01-07 15:06:17 -0800

    add Faker and setup seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  product = Product.create!(name: Faker::Games::Minecraft.item, cost: Faker::Number.within(range: 1..1000), country_of_origin: Faker::Games::LeagueOfLegends.location)
+  5.times do |index|
+    Review.create!(author: Faker::JapaneseMedia::OnePiece.character, content_body: Faker::Lorem.sentence(word_count: 20), rating: Faker::Number.within(range: 1..5), product_id: product.id)
+  end
+end
