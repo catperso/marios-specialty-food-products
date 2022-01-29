b87c4339b515f9818d31711cd2ddf242fddd81a0
Matt Casperson <mc.casperson@gmail.com>
2022-01-29 15:06:48 -0800

    add spec for non-admin view differences

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 describe "the Delete a Product process" do
 
   before(:each) do
-    admin = User.create!(:email => 'Capy@Bara.com', :password => 'epicodus', :admin => true)
-    login_as(admin, :scope => :user)
+    @product = Product.create(name: "Mushroom Stew", cost: "69", country_of_origin: "USA")
   end
 
   it "deletes a product" do
-    product = Product.create(name: "Mushroom Stew", cost: "69", country_of_origin: "USA")
-    visit product_path(product)
+    admin = User.create!(:email => 'Capy@Bara.com', :password => 'epicodus', :admin => true)
+    login_as(admin, :scope => :user)
+    visit product_path(@product)
     click_on 'Delete product'
     expect(page).to have_no_content "Mushroom Stew"
     expect(page).to have_content "Product removed successfully!"
+  end
+
+  it "is unavailable to non-admin users" do
+    user = User.create!(email: 'bappy@cara.com', password: 'epicodus', admin: false)
+    login_as(user, scope: :user)
+    visit product_path(@product)
+    expect(page).to have_no_content "Delete product"
   end
 end
--- spec/features/delete_review_pages_spec.rb
+++ spec/features/delete_review_pages_spec.rb
@@ -3,16 +3,23 @@ require 'rails_helper'
 describe "the Delete a Review process" do
 
   before(:each) do
-    admin = User.create!(:email => 'Capy@Bara.com', :password => 'epicodus', :admin => true)
-    login_as(admin, :scope => :user)
+    @product = Product.create(name: "Mushroom Stew", cost: "69", country_of_origin: "USA")
+    @review = Review.create(author: "Luigi", content_body: "Now we can create integration testing files. Let's create a few examples to test CRUD functionality for adding albums", rating: '5', product_id: @product.id)
   end
 
   it "deletes a review" do
-    product = Product.create(name: "Mushroom Stew", cost: "69", country_of_origin: "USA")
-    review = Review.create(author: "Luigi", content_body: "Now we can create integration testing files. Let's create a few examples to test CRUD functionality for adding albums", rating: '5', product_id: product.id)
-    visit product_review_path(product, review)
+    admin = User.create!(:email => 'Capy@Bara.com', :password => 'epicodus', :admin => true)
+    login_as(admin, :scope => :user)
+    visit product_review_path(@product, @review)
     click_on 'Delete this review'
     expect(page).to have_no_content "Luigi"
     expect(page).to have_content "Review removed successfully!"
+  end
+
+  it "is unavailable for non-admin users" do
+    user = User.create!(email: 'bappy@cara.com', password: 'epicodus', admin: false)
+    login_as(user, scope: :user)
+    visit product_review_path(@product, @review)
+    expect(page).to have_no_content 'Delete this review'
   end
 end
